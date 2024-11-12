17a203e37886bc5c375b5897524c403684edfa01
V1rsk1 <dudkam228@gmail.com>
2024-11-12 17:38:31 +0200

    Добавьте файлы проекта.

---
+++ clssstr/clssstr.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+void StR(string str) {
+    cout << "F:\\HTML_P34\\menuuu_p34_3\\lesson5_homework\\ex1.png" << endl;
+    if (str.rfind("\\") == str.length()) {
+        cout << "";
+    }
+    else {
+        cout << "\nPath without file name: ";
+        for (short i = 0; i < str.rfind("\\"); i++) {
+            cout << str[i];
+        }
+    }
+    cout << endl << "\nLast name folder in path: ";
+    for (short i = str.rfind("\\", str.rfind("\\") - 1) + 1; i < str.rfind("\\"); i++) {
+        cout << str[i];
+    }
+    cout << endl << "\nFile name with extension: " << str.substr(str.rfind("\\") + 1) << endl;
+    cout << "\nExtension file name: " << str.substr(str.rfind(".")) << endl;
+    cout << "\nFile name without extension: ";
+    for (short i = str.rfind("\\") + 1; i < str.rfind("."); i++) {
+        cout << str[i];
+    }
+}
+int main()
+{
+    StR("F:\\HTML_P34\\menuuu_p34_3\\lesson5_homework\\ex1.png");
+}
